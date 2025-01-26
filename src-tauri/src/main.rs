b73d67f3dc6b53990a5a9d20be7a99327a9d9fe4
BigIskander <bigiskander@gmail.com>
2025-01-26 20:19:38 +0300

    Add description.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,14 @@
 
 fn main() {
     tauri::Builder::default()
+        .setup(|_| {
+            println!("Handwriting keyboard for Linux X11 desktop environment. \n\
+                        Currently supports Chinese language only. \n\
+                        To recognize handwritten pattern program uses Google API. \n\
+                        Github page: \n\
+                        https://github.com/BigIskander/Handwriting-keyboard-for-Linux./tree/main");
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
