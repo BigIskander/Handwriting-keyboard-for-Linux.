3c2b6f23b8a37b8f2268ef0a241d345d1f28f9c9
BigIskander <bigiskander@gmail.com>
2025-03-18 22:32:22 +0300

    Window handling.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,30 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
+use gtk::prelude::GtkWindowExt;
+use tauri::Manager;
+use tauri::LogicalSize;
+
+// workaround
+// set_accept_focus(false) property doesn't work in main window
+#[tauri::command]
+fn open_keyboard_window(app: tauri::AppHandle) {
+    let url = tauri::WebviewUrl::App("keyboard.html".into());
+    let window = tauri::webview::WebviewWindowBuilder::new(&app, "local", url)
+        .use_https_scheme(true)
+        .build()
+        .unwrap();
+    window.set_title("手写").unwrap();
+    window.set_always_on_top(true).unwrap();
+    let min_size: LogicalSize<u32> = tauri::LogicalSize::from((800, 300));
+    window.set_min_size(Some(min_size)).unwrap();
+    window.set_size(min_size).unwrap();
+    let gtk_window = window.gtk_window().unwrap();
+    gtk_window.set_accept_focus(false);
+    window.show().unwrap();
+    app.get_webview_window("main").unwrap().close().unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
@@ -17,6 +41,7 @@ fn main() {
                       -------------------------------------------------------------------------------");
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![open_keyboard_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import { invoke } from '@tauri-apps/api/core';
+invoke('open_keyboard_window');
